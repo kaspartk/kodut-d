b0d74090f7eedaaa5e14d93125fee6a20cfbef30
kaspartk <71014237+kaspartk@users.noreply.github.com>
2021-10-27 19:47:10 +0300

    Delete src directory

--- src/main/java/ee/mihkel/backend/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package ee.mihkel.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ee.mihkel.backend.model.Category;
-
-@Repository
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
